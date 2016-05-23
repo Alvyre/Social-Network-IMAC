decbd611dea8c8be44d9d390e7b73fe7929e6cf7
Tencap <noisiv@outlook.fr>
2016-05-23 17:02:18 +0200

    delete useless stuffs

--- src/public/api/index.php
+++ src/public/api/index.php
@@ -1,40 +1,18 @@
-<?php 
+<?php
+
+// use \Psr\Http\Message\ServerRequestInterface as Request;
+// use \Psr\Http\Message\ResponseInterface as Response;
 
 require './vendor/autoload.php';
+require './config.php';
 
-// Prepare app
-$app = new \Slim\Slim(array(
-    'templates.path' => './templates',
-));
+// instantiate the App object
+$app = new \Slim\App();
 
-// Create monolog logger and store logger in container as singleton 
-// (Singleton resources retrieve the same log resource definition each time)
-$app->container->singleton('log', function () {
-    $log = new \Monolog\Logger('slim-skeleton');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler('./logs/app.log', \Monolog\Logger::DEBUG));
-    return $log;
-});
-
-// Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('./templates/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
-
-// Define routes
 $app->get('/', function () use ($app) {
-    // Sample log message
-    $app->log->info("Slim-Skeleton '/' route");
-    // Render index view
     $app->render('index.html');
 });
 
-// Run app
 $app->run();
 
 ?>
