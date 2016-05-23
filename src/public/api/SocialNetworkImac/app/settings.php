bab55ec147781fed499837aa7590af0d485b8599
Tencap <noisiv@outlook.fr>
2016-05-24 00:14:44 +0200

    new structure added for api

---
+++ src/public/api/SocialNetworkImac/app/settings.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'settings' => [
+        // Slim Settings
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => true,
+
+        // View settings
+        'view' => [
+            'template_path' => __DIR__ . '/templates',
+            'twig' => [
+                'cache' => __DIR__ . '/../cache/twig',
+                'debug' => true,
+                'auto_reload' => true,
+            ],
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'app',
+            'path' => __DIR__ . '/../log/app.log',
+        ],
+
+        // Eloquent database settings
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'imac-network',
+            'username' => 'root',
+            'password' => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+    ],
+];
