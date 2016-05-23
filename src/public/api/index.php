6b48432f0709b275654e8b7dcdaf5a0817c84cd8
Arctic76 <belum76@gmail.com>
2016-05-23 14:56:58 +0200

    Moved Slim into API folder

--- src/public/api/index.php
+++ src/public/api/index.php
@@ -1,17 +1,17 @@
 <?php 
 
-require '../vendor/autoload.php';
+require './vendor/autoload.php';
 
 // Prepare app
 $app = new \Slim\Slim(array(
-    'templates.path' => '../templates',
+    'templates.path' => './templates',
 ));
 
 // Create monolog logger and store logger in container as singleton 
 // (Singleton resources retrieve the same log resource definition each time)
 $app->container->singleton('log', function () {
     $log = new \Monolog\Logger('slim-skeleton');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Monolog\Logger::DEBUG));
+    $log->pushHandler(new \Monolog\Handler\StreamHandler('./logs/app.log', \Monolog\Logger::DEBUG));
     return $log;
 });
 
@@ -19,7 +19,7 @@ $app->container->singleton('log', function () {
 $app->view(new \Slim\Views\Twig());
 $app->view->parserOptions = array(
     'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
+    'cache' => realpath('./templates/cache'),
     'auto_reload' => true,
     'strict_variables' => false,
     'autoescape' => true
