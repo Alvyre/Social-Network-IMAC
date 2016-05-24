f3da4d499c25ea60358628407e51668e76f8a6c8
Hellowizz <heloroussel@hotmail.fr>
2016-05-24 12:18:15 +0200

    Database connexion
    Il est désormais possible de se connecter à une database

--- src/public/api/Database.php
+++ src/public/api/Database.php
@@ -1,1 +1,31 @@
-// COMMENTAIRE INUTILE YEAH
+
+<?php
+
+	// OUVERTURE DE FICHIER
+// On ouvre le fichier
+
+	// DEDANS IL Y A :
+		// localhost
+		// user
+		// password 
+		// Nom
+
+$monfichier = fopen('connexion.txt', 'r');
+
+// On fait ici nos opérations sur le fichier...
+$localhost = fgets($monfichier);
+$user = fgets($monfichier);
+$password = fgets($monfichier);
+$name = fgets($monfichier);
+
+$db;
+
+    function __construct()
+    {
+        $this->db = new mysqli($this->localhost, $this->user, $this->password, $this->name);
+    }
+
+// On a fini de l'utiliser, on ferme le fichier
+fclose($monfichier);
+
+?>
