89f13c8b290efe08bbc2df96a196b6fc838b62c0
Tencap <noisiv@outlook.fr>
2016-05-24 14:47:44 +0200

    comment query

--- api/app/routes.php
+++ api/app/routes.php
@@ -1,31 +1,33 @@
 <?php
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-// Routes
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name ssoulet");
-
-    return $response;
-});
 
 //accueil
 $app->get('/', App\Action\HomeAction::class)
  	->setName("home");
 
-//all datas -> test
 //categories
 $app->get('/category', App\Action\CategoryAction::class)
     ->setName("category");
+
 //comments
-$app->get('/comment', App\Action\CommentAction::class)
-	->setName("comment");
+$app->get('/comment-getall', 'App\Action\CommentAction:readAll')
+	->setName("comment-getall");
+$app->get('/comment-get', 'App\Action\CommentAction:readOne')
+	->setName("comment-get");
+$app->get('/comment-create', 'App\Action\CommentAction::create')
+	->setName("comment-create");
+$app->get('/comment-update', 'App\Action\CommentAction::update')
+	->setName("comment-update");
+$app->get('/comment-delete', 'App\Action\CommentAction::delete')
+	->setName("comment-delete");
+
 //subjects
 $app->get('/subject', App\Action\SubjectAction::class)
 	->setName("subject");
+
 //users
 $app->get('/user', 'App\Action\UserAction:readAll')
 	->setName("user");
+
 //vote
 $app->get('/vote', App\Action\VoteAction::class)
 	->setName("vote");
