f716dbb0901a50a61d3773ed336c1e93d753539d
maureenroche <roche.maureen@gmail.com>
2016-05-25 14:19:51 +0200

    Traitement formulaire [en cours]

--- api/Controllers/UserControl.php
+++ api/Controllers/UserControl.php
@@ -1,10 +1,33 @@
 <?php
 
+$fonction = $_GET['fonction'];
+
+switch ($fonction) {
+  case 'login':
+  login();
+  break;
+  case 'createUser':
+  createUser();
+  break;
+
+  default:
+  echo "probleme";
+  break;
+}
 
 
+function createUser(){
+  if (!empty($_POST)) {
+    if (!isset($_POST['PseudoUser']) || $_POST['PseudoUser'] == '') {
+      $PseudoUser = real_escape_string($_POST['PseudoUser']);
+    }
 
+  }
 
+}
 
+function login(){
 
+}
 
 ?>
