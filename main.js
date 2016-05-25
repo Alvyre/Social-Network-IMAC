899fde555f523a8c41f0f23fb9ef457f91603716
Arctic76 <belum76@gmail.com>
2016-05-25 10:41:22 +0200

    fix commit to pedro

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 // main.js
 
 import Vue from 'vue'
-import Bootstrap from 'bootstrap'
 import Resource from 'vue-resource'
 import Router from 'vue-router'
 
 
 
 import App from './components/App.vue'
-import HomeView from './components/HomeView.vue'
-import ProfileView from './components/ProfileView.vue'
-import CategoryView from './components/CategoryView.vue'
-import SubjectView from './components/SubjectView.vue'
-import SignUpView from './components/SignUpView.vue'
+import HomeView from './views/HomeView.vue'
+import ProfileView from './views/ProfileView.vue'
+import CategoryView from './views/CategoryView.vue'
+import SubjectView from './views/SubjectView.vue'
+import SignUpView from './views/SignUpView.vue'
 
 // install router
 Vue.use(Router)
--- views/SignUpView.vue
+++ views/SignUpView.vue
@@ -1,44 +1,59 @@
 <template>
-  <div class="inscription">
-    <form action="" method="POST" role="form">
-      <legend>Inscription</legend>
-    
-      <div class="form-group">
-        <label for="pseudo">Pseudo</label>
-        <input type="text" name="pseudo" class="form-control" id="pseudo" placeholder="Input your pseudo">
-        <label for="pwd">Mot de passe</label>
-        <input type="password" name="password" class="form-control" id="password">
-        <label for="status">Status</label>
-        <input type="text" name="status" class="form-control" id="status" placeholder="IMAC201X, Prof, ...">
-        <label for="mail">e-mail</label>
-        <input type="mail" name="mail" class="form-control" id="mail" placeholder="Input your mail">
-        <label for="sex"> Sexe</label>
-        <select name="sex" id="sex">
-          <option value="Homme">Homme</option>
-          <option value="Femme" selected>Femme</option>
-        </select>
-        <label for="bio">Informations Supplémentaires :</label>
-        <textarea id="bio" name ="bio"></textarea>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3">
+      <div class="inscription">
+        <form action="" method="POST" role="form">
+          <legend>Inscription</legend>
+        
+          <div class="form-group">
+          <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <strong>Ce champs est nécéssaire</strong>
+          </div>
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" name="pseudo" class="form-control" id="pseudo" placeholder="Pseudo" required>
+            </div>
+          <div class="form-group">
+            <label for="pwd">Mot de passe :</label>
+            <input type="password" name="password" class="form-control" id="password" required>
+          </div>
+          <div class="form-group">
+            <label for="status">Status :</label>
+            <input type="text" name="status" class="form-control" id="status" placeholder="IMAC201X, Prof, ..." required>
+          </div>
+          <div class="form-group">
+            <label for="mail">E-mail :</label>
+            <input type="mail" name="mail" class="form-control" id="mail" placeholder="foo@mail.com" required>
+          </div>
+          <div class="form-group">
+            <label for="sex"> Sexe :</label>
+            <select name="sex" id="sex">
+              <option value="Homme">Homme</option>
+              <option value="Femme" selected>Femme</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="bio">Informations Supplémentaires :</label>
+            <textarea id="bio" name ="bio" v-model="message" placeholder="Portfolio, links, .."></textarea>
+          </div>
+          
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
-    
-      
-    
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-  </div>
+    </div>
   </div>
 </template>
 
+
+
 <script>
-
-
 export default {
   components: {
+    
   }
 }
 </script>
 <!-- GET DATA JSON
-
 ,
   data() {
     return {
@@ -62,6 +77,4 @@ export default {
 -->
 
 <style type="text/css">
-
 </style>
-
