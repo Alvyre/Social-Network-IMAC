cb1c43a255b723d337b086c6ac9a990b089f1802
Hellowizz <heloroussel@hotmail.fr>
2016-05-25 14:45:40 +0200

    Nouvelles fonctions dans UserControl

--- api/Controllers/UserControl.php
+++ api/Controllers/UserControl.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'Models/User.php';
+
 $fonction = $_GET['fonction'];
 
 switch ($fonction) {
@@ -21,12 +23,32 @@ function createUser(){
     if (!isset($_POST['PseudoUser']) || $_POST['PseudoUser'] == '') {
       $PseudoUser = real_escape_string($_POST['PseudoUser']);
     }
-
+    if (!isset($_POST['PassUser']) || $_POST['PassUser'] == '') {
+      $PassUser = real_escape_string($_POST['PassUser']);
+    }
+    if (!isset($_POST['EmailUser']) || $_POST['EmailUser'] == '') {
+      $EmailUser = real_escape_string($_POST['EmailUser']);
+    }
+    if (!isset($_POST['StatusUser']) || $_POST['StatusUser'] == '') {
+      $StatusUser = real_escape_string($_POST['StatusUser']);
+    }
+    if (!isset($_POST['SexUser']) || $_POST['SexUser'] == '') {
+      $SexUser = real_escape_string($_POST['SexUser']);
+    }
+    if (!isset($_POST['BioUser']) || $_POST['BioUser'] == '') {
+      $BioUser = real_escape_string($_POST['BioUser']);
+    }
   }
-
 }
 
 function login(){
-
+  if (!empty($_POST)) {
+    if (!isset($_POST['PseudoUser']) || $_POST['PseudoUser'] == '') {
+      $PseudoUser = real_escape_string($_POST['PseudoUser']);
+    }
+    if (!isset($_POST['PassUser']) || $_POST['PassUser'] == '') {
+      $PassUser = real_escape_string($_POST['PassUser']);
+    }
+  }
 }
 ?>
