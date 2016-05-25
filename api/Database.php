eaef376561320e0d441c419c5e0bfa346f6d287a
maureenroche <roche.maureen@gmail.com>
2016-05-25 11:38:41 +0200

    Lecture fichier Congif BDD

--- api/Database.php
+++ api/Database.php
@@ -1,31 +1,13 @@
 <?php
-include("Config.php");
+include 'Config.php';
 
 /****************************
 Connexion à la base de donnée
 *****************************/
 function connexionBDD()
 {
-
-  //Lecture du fichier de connexion à la base de donnée
-
-  // le fichier doit être écrit comme ça :
-  // localhost
-  // user
-  // password
-  // Nom
-
-  // $monfichier = fopen('connexion.txt', 'r');
-  //
-  // // On fait ici nos opérations sur le fichier...
-  // $localhost = fgets($monfichier);
-  // $user = fgets($monfichier);
-  // $password = fgets($monfichier);
-  // $BDDname = fgets($monfichier);
-
-
   //Connexion à la base de donnée
-  $mysqli = new mysqli($connexion[0], $connexion[1], $connexion[2], $connexion[3]);
+  $mysqli = new mysqli(HOST, USER, PSWD, BDD);
 
   if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
@@ -33,12 +15,6 @@ function connexionBDD()
   }
   $mysqli->set_charset("utf8");
 
-  // // On a fini de l'utiliser, on ferme le fichier
-  // fclose($monfichier);
-
   return $mysqli;
 }
-
-
-connexionBDD();
 ?>
