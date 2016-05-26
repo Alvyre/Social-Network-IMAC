fc2bdb66e2b8db5ee9b796f0ea697933cb6a60df
Alice <alice.maixent@gmail.com>
2016-05-26 18:28:38 +0200

    category ok

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ export function apiRoot(){
     /* REPLACE WITH YOUR LOCALHOST PROJECT ROOT */
     /* Exemple : "http://localhost:8888/Social-Network-IMAC/" */
 
-    var projectRoot = "http://localhost:8888/Projetweb/Social-Network-IMAC/";
+    var projectRoot = "http://localhost/Social-Network-IMAC/";
 
     return projectRoot + "api/public/";
 }
