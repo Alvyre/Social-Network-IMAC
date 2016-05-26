1ab0686090220485c1289a0e4cf6f0c3765f04e9
CoralieGold <coralie.gold@gmail.com>
2016-05-26 20:33:50 +0200

    Fonction get subjects à partir d'id category

--- api/Controllers/Category/getSubjectsCategory.php
+++ api/Controllers/Category/getSubjectsCategory.php
@@ -1,6 +1,12 @@
 <?php 
-	$category = readTitleCategories($_POST["title"]);
+	require_once(__DIR__."/../../Models/Category.php");
+	require_once(__DIR__."/../../Models/Subject.php");
+	$category = readIdCategories($_POST["id"]);
 	$sujets = readAllSubjectsCategory($category[0]["IdCat"]); 
-	json_encode($sujets);
-	echo $sujets;
+	$retour = [ 
+	"idCat" => $category[0]["IdCat"], 
+	"titleCat" => $category[0]["TitleCat"],
+	"sub" => $sujets
+	];
+	echo "[".json_encode($retour)."]";
 ?>
